e40f825e85a6c4cfce26c065e663212cb124ed46
jeongin <kwjddls1919@naver.com>
2023-01-15 10:55:14 +0900

    영화 목록 기능 추가

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+import MovieCard from "../components/MovieCard";
+import { loadMovie } from "../reducers/movie";
 
-const index = () => {
-  return <>index</>;
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const { movieList, loadMovieDone } = useSelector((state) => state.movie);
+
+  useEffect(() => {
+    dispatch(loadMovie());
+  }, []);
+
+  if (loadMovieDone) {
+    return (
+      <Wrapper>
+        {movieList.map((i) => (
+          <MovieCard key={i.id} movie={i} />
+        ))}
+      </Wrapper>
+    );
+  }
 };
 
-export default index;
+export default Home;
